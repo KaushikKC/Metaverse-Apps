726dd515d66b75e5725ba90c4704f29af4e238d4
KaushikKC <kccreations1704@gmail.com>
2022-08-03 14:58:45 +0530

    Completed Frontend

--- nft-project/pages/nft/[id].tsx
+++ nft-project/pages/nft/[id].tsx
@@ -7,6 +7,7 @@ function NFTDropPage() {
     const connectWithMetamask = useMetamask();
     const address = useAddress();
     const disconnect = useDisconnect();
+    console.log(address)
 
 
   return (
@@ -36,9 +37,14 @@ function NFTDropPage() {
                 <h1 className='w-52 cursor-pointer text-xl font-extralight sm:w-80'>
                     The{' '} <span className='font-extrabold underline decoration-pink-600/50'>PAPAFAM</span>{' '} NFT Market Place
                 </h1>
-                <button onClick={() => connectWithMetamask()} className="rounded-full bg-rose-400 px-4 py-2 text-xs font-bold text-white lg:px-5 lg:py-3 lg:text-base">Sign In</button>
+                <button onClick={() => (address ? disconnect() : connectWithMetamask())} className="rounded-full bg-rose-400 px-4 py-2 text-xs font-bold text-white lg:px-5 lg:py-3 lg:text-base">
+                    {address ? 'Sign Out' : 'Sign In'}
+                </button>
             </div>
             <hr className="my-2 border" />
+            {address && (
+                <p className='text-center text-sm text-rose-400'>You're Logged in with wallet {address.substring(0,5)}...{address.substring(address.length - 5)}</p>
+            )}
             {/* Container */}
             <div className='mt-10 flex flex-1 flex-col items-center space-y-6 text-center lg:space-y-0 lg:justify-center'>
                 <img className='w-80 object-cover pb-10 lg:h-40' src="https://links.papareact.com/bdy" alt="" />
